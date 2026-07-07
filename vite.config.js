691be2a5b883397dd9aa0c0ef5d57a93b534753c
mihaelaa6 <mihaelaa6@gmail.com>
2026-07-07 10:32:52 -0600

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal.github.io.git/', // ⚠️ Replace this with your exact GitHub repo name!
+  base: '/personal.github.io/', // ⚠️ Replace this with your exact GitHub repo name!
 })
